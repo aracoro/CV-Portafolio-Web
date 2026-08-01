3c7f427284ab398bbeec609e56625b253ce46c6e
Araceli  Puerta Coronado <aracelipuert@gmail.com>
2026-07-31 22:18:00 -0700

    Actualizacion de codigo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-hvhv
+console.log("%c Araceli Puerta | Network & Infrastructure Portfolio ", "background: #0d131f; color: #00f2fe; font-size: 14px; font-weight: bold; padding: 5px; border: 1px solid #00f2fe;");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const contactForm = document.getElementById('contact-form');
+
+    if (contactForm) {
+        contactForm.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            alert('[PACKET DELIVERED]: Tu mensaje ha sido enviado correctamente. Gracias por contactarme, Araceli se pondrá en contacto contigo pronto.');
+
+            contactForm.reset();
+        });
+    }
+});
